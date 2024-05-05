ef2cced0b2d782bdd820ac4f71bb700af0ff14be
Saksham Khare <sakshamkhare19@gmail.com>
2024-05-05 16:04:43 +0530

    input change

--- src/UsernameForm.jsx
+++ src/UsernameForm.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 function UsernameForm () {
     const [username, setUsername] = useState("Tyrex");
+
+    const updateUsername = (evt) => {
+        setUsername(evt.target.value);
+    }
+    
     return (
         <div>
-            <input type="text" placeholder="username" value={username} />
+            <input
+                type="text" 
+                placeholder="username" 
+                value={username}
+                onChange={updateUsername} 
+            />
             <button>Submit</button>
         </div>
     )
