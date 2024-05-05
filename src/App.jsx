5fe81276d51e4c103aa821e7ee01bcfaffe70a16
Saksham Khare <sakshamkhare19@gmail.com>
2024-05-05 19:19:47 +0530

    list of inventory

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import UsernameForm from './UsernameForm';
 import SignupForm from './SignupForm';
 import BetterSignup from './BetterSignup';
 import ShoppingListForm from './ShoppingListForm';
+import ShoppingList from './ShoppingList';
 
 function App() {
 
   return (
     <>
+      <ShoppingList />
       <ShoppingListForm />
       {/* <BetterSignup /> */}
       {/* <SignupForm /> */}
--- src/ShoppingListForm.jsx
+++ src/ShoppingListForm.jsx
@@ -36,6 +36,7 @@ function ShoppingListForm () {
                 id="quantity"
                 value={formData.quantity}
             />
+            <button>Add Item</button>
         </form>
     )
 }
